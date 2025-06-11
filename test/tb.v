55ae5f562ae22ef85eaa73d9851311eda42f713a
rawhan10 <rohanloni50@gmail.com>
2025-06-11 22:34:22 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -30,7 +30,7 @@ module tb ();
    `endif
 
   // Replace tt_um_example with your module name:
-  tt_10_ihp user_project (
+  tt_um_10_ihp user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
