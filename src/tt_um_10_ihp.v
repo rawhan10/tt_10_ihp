e85fe1530ab39f2b72f5eebd99419fcbca3af78a
rawhan10 <rohanloni50@gmail.com>
2025-06-11 22:34:00 +0530

    Update and rename tt_10_ihp.v to tt_um_10_ihp.v

--- src/tt_um_10_ihp.v
+++ src/tt_um_10_ihp.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_10_ihp (
+module tt_um_10_ihp (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
